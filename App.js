82bdde33692c9286c3163825269c2508ecdcee75
unknown <kalle.kinnunen@myy.haaga-helia.fi>
2022-09-03 12:38:29 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+
 
 export default function App() {
+  const [result, setResult] = React.useState('')
+  const [firstNumber, setFirstNumber] = React.useState('')
+  const [secondNumber, setSecondNumber] = React.useState('')
+  
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.heading}>Result: {result} </Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        value={firstNumber} onChangeText={first => setFirstNumber(first)}></TextInput>
+
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        value={secondNumber} onChangeText={second => setSecondNumber(second)}></TextInput>
+
+      <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setResult(parseInt(firstNumber) + parseInt(secondNumber))}>
+          <Text style={styles.buttontext}>+</Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setResult((firstNumber - secondNumber))}>
+          <Text style={styles.buttontext}>-</Text>
+        </TouchableOpacity>
+
+      </View>
+
+ <StatusBar style="auto" />
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 200,
+    paddingBottom: 20
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginRight: 20,
+    marginLeft: 20,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  buttontext: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
+
+  },
+  heading: {
+    color: 'blue',
+    fontSize: 20,
+    paddingBottom: 20,
+
   },
 });
